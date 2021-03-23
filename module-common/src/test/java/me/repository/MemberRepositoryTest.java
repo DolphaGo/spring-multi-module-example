c0383bbc434688903176b49b983ec54e909c63a9
DolphaGo <adamdoha@naver.com>
2021-03-24 01:41:15 +0900

    Refactor

--- module-common/src/test/java/me/repository/MemberRepositoryTest.java
+++ module-common/src/test/java/me/repository/MemberRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,5 +20,6 @@ public class MemberRepositoryTest {
         memberRepository.save(new Member("DolphaGo", "adamdoha@naver.com"));
         Optional<Member> saved = memberRepository.findById(1L);
         assertThat(saved.get().getName()).isEqualTo("DolphaGo");
+        assertThat(saved.get().getEmail()).isEqualTo("adamdoha@naver.com");
     }
 }
